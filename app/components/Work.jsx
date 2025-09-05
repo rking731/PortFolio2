e60b61426ead0dc28d3d1fc3a53c41c6a436cb00
Kingshuk Roy <rkingshuk731@gmail.com>
2025-09-06 00:47:27 +0530

    Updated project with new features

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -21,7 +21,7 @@ const Work = () => {
                           <motion.div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 
                           group-hover:bottom-7'>
                             <div>
-                            <h2 className='font-semibold'>{project.title}</h2>
+                            <h2 className='font-semibold text-black'>{project.title}</h2>
                             <p className='text-sm text-gray-700 '>{project.description}</p>
                            </div>
                            <div className='border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition'>
